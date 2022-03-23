fb8f9114ee8def9fe56df6da06b77dbb1470e6a2
andrarusea <ruseaandra19@stud.ase.ro>
2022-03-23 21:03:29 +0200

    added singleton

---
+++ Singleton/src/acs/ase/ro/Main.java
@@ -0,0 +1,13 @@
+package acs.ase.ro;
+
+public class Main {
+
+	public static void main(String[] args) {
+		SoferAutobuz soferAutobuz1 = SoferAutobuz.getInstance("Nume1");
+		SoferAutobuz soferAutobuz2 = SoferAutobuz.getInstance("Nume2");
+		System.out.println(soferAutobuz1.getNume());
+		System.out.println(soferAutobuz2.getNume());
+
+	}
+
+}
